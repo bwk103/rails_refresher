c9f2dd0ae960ad0d854ec3595aebd0ba7484c228
Ben Kielty <bwk103@gmail.com>
2018-02-04 22:01:40 +0000

    Refactor Contact/Home

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,54 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Sample App"
+  end
+
+  test "root should be home" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
   end
 
+  test "should have home title" do
+    get static_pages_home_url
+    assert_select "title", "Home | " + @base_title
+  end
+
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+  end
+
+  test "should have help title" do
+    get static_pages_help_url
+    assert_select "title", "Help | " + @base_title
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should have about title" do
+    get static_pages_about_url
+    assert_select "title", "About | " + @base_title
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+  test "should have contact title" do
+    get static_pages_contact_url
+    assert_select "title", "Contact | " + @base_title
   end
 
 end

0ea540f961304b7812a977ceff1a1bd56d7c1b52
Ben Kielty <bwk103@gmail.com>
2018-02-07 18:02:03 +0000

    Added authorization

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,35 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
+
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
+  test 'should redirect edit when not logged in' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@user)
+    get edit_user_path(@other_user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged ina s wrong user' do
+    log_in_as(@user)
+    patch user_path(@other_user)
+    assert_redirected_to root_url
+    assert flash.empty?
+  end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'invalid edits' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: {
@@ -20,6 +21,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'valid edits' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: {
@@ -32,5 +34,35 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     @user.reload
     assert_equal @user.name, 'Mike Example'
+  end
+
+  test 'valid edit with friendly forwarding' do
+    get edit_user_path(@user)
+    assert_redirected_to login_path
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    post login_path, params: { session: {
+                                          email: @user.email,
+                                          password: 'password'
+      }}
+    assert_redirected_to edit_user_path(@user)
+  end
+
+  test 'friendly forwarding only stores location once' do
+    get edit_user_path(@user)
+    assert_redirected_to login_path
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    post login_path, params: { session: {
+                                          email: @user.email,
+                                          password: 'password'
+      }}
+    delete logout_path
+    assert_not is_logged_in?
+    get login_path
+    post login_path, params: { session: {
+                                          email: @user.email,
+                                          password: 'password'
+      }}
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
   end
 end

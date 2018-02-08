316156c0834779dfbe0366105a9d2ac4214729c0
Ben Kielty <bwk103@gmail.com>
2018-02-08 22:30:27 +0000

    Created microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -82,4 +82,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember,'')
   end
 
+  test 'associated microposts should be destroyed' do
+    @user.save
+    @user.microposts.create!(content: "This is a post")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end

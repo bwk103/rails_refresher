06e1a898a0b6739fc5f1050efb49b6cee8230ec2
Ben Kielty <bwk103@gmail.com>
2018-02-08 09:06:52 +0000

    Finish user edit, update, index, delete

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test 'should redirect index when not logged in' do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test 'should redirect edit when not logged in' do
     get edit_user_path(@user)
     assert_not flash.empty?
@@ -31,5 +36,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@other_user)
     assert_redirected_to root_url
     assert flash.empty?
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              'password',
+                                            password_confirmation: 'password',
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@other_user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in but not admin' do
+    assert_no_difference 'User.count' do
+      log_in_as(@other_user)
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,18 +2,35 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test 'layout links' do
     get root_path
     assert_template('static_pages/home')
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
     assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select 'title', 'Contact | Ruby on Rails Sample App'
     get signup_path
     assert_select 'title', 'Signup | Ruby on Rails Sample App'
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", contact_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 
 end

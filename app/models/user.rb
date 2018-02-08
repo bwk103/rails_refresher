46d6bff90512f083ea637b9f4930275a111440a4
Ben Kielty <bwk103@gmail.com>
2018-02-08 10:14:02 +0000

    Added mailer for Account Activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_create :create_activation_digest
+  before_save :downcase_email
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -32,5 +34,16 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+      email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
-User.create!(name:'Bob Smith', email: 'bob@test.com', password: 'password',
-            password_confirmation: 'password', admin: true)
+User.create!(name:'Bob Smith',
+             email: 'bob@test.com',
+             password: 'password',
+             password_confirmation: 'password',
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create(name: name, email: email, password: password,
-              password_confirmation: password)
+  User.create(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
